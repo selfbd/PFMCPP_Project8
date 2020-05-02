378c4aa08bb32da65e66dc9ba7a816da6fd080e2
selfbd <bds_github@fastmail.fm>
2020-05-02 16:47:27 +0000

    Add dtor to Car.h; Added copy constructor and copy assignment operator to Car.h and Car.cpp; Removed forward declare of Vehicle in Car.h, replaced it with an include of Vehicle insead; Added Car.cpp to .replit;

--- Car.h
+++ Car.h
@@ -1,11 +1,15 @@
 #pragma once
 
-struct Vehicle;
+#include "Vehicle.h"
 
 struct Car : public Vehicle
 {
     Car(const std::string& s);
+    ~Car() override;
+    Car(const Car&);
+    Car& operator=(const Car&);
+
     void closeWindows();
 
-    void tryToEvade();
+    void tryToEvade() override;
 };
